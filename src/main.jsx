6aba0bb9ce0d5f043b09f93a5811fad329552c47
jennifer hodge <jennifer.hodge@russdarrow.com>
2026-09-16 09:34:29 -0500

    adding protected routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import DashBoard from "./Components/DashBoard";
 import Recipes from "./Components/Recipes.jsx";
 import Workouts from "./Components/Workouts.jsx";
 import Favorites from "./Components/Favorites.jsx";
-
 import Legal from "./Components/Legal.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -15,12 +15,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/login" element={<FuelLeanLogin />} />
       <Route path="/legal" element={<Legal />} />
       <Route path="/" element={<FuelLeanLogin />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/workouts" element={<Workouts />} />
-      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+      <Route path="/dashboard" element={<ProtectedRoute><DashBoard /></ProtectedRoute>} />
+      <Route path="/recipes" element={<ProtectedRoute><Recipes /></ProtectedRoute>} />
+      <Route path="/workouts" element={<ProtectedRoute><Workouts /></ProtectedRoute>} />
+      <Route path="/favorites" element={<ProtectedRoute><Favorites /></ProtectedRoute>} />
     </Routes>
   </BrowserRouter>,
 );
